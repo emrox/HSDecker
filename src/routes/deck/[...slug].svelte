349221668dd9caacd1f7398eefb25aa1ead087c1
Stefan Bauckmeier <emrox@elektronische-musik.de>
2020-03-19 20:21:20 +0100

    fix: deal with slashes in code
    page was just rendering when using the `goto` method, not when accessing the url

---
+++ src/routes/deck/[...slug].svelte
@@ -0,0 +1,16 @@
+<script context="module">
+  import { encode, decode, FormatType } from 'deckstrings';
+
+  export async function preload(page, session) {
+    const { slug } = page.params;
+    const deckCode = slug.join('/');
+
+    return { deckCode };
+  }
+</script>
+
+<script>
+  export let deckCode;
+</script>
+
+<h1>{deckCode}</h1>
