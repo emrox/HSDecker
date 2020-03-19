47f5a2536ab2fa90c52217803a2aae9e6357c718
Stefan Bauckmeier <emrox@elektronische-musik.de>
2020-03-19 16:44:21 +0100

    feat: show imaginary deck

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -25,6 +25,8 @@
 
 <script>
   export let cards;
+
+  export const randomDeck = cards.sort(() => 0.5 - Math.random()).slice(0, 30)
 </script>
 
 <style>
@@ -34,4 +36,16 @@
 	<title>HSdecker</title>
 </svelte:head>
 
-<h1>Hello World {cards}</h1>
+<h1>Random Deck</h1>
+
+<ul>
+  {#each randomDeck as card}
+    <li class="card">
+      <h3>{card.name}</h3>
+      <blockquote>{@html card.flavor}</blockquote>
+      {#if card.cost}<span class="card__cost">{card.cost}</span>{/if}
+      {#if card.attack}<span class="card__attack">{card.attack}</span>{/if}
+      {#if card.durability}<span class="card__durability">{card.durability}</span>{/if}
+    </li>
+  {/each}
+</ul>
