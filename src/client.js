6f2c86e6c986617ded1469aa4350cce11ad313bb
Stefan Bauckmeier <emrox@elektronische-musik.de>
2020-03-24 15:18:54 +0100

    chore: Firebase setup

--- src/client.js
+++ src/client.js
@@ -1,5 +1,13 @@
 import * as sapper from '@sapper/app';
 
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { firebaseConfig } from '../firebase.config';
+
+firebase.initializeApp(firebaseConfig);
+
 sapper.start({
   target: document.querySelector('#sapper')
 });
