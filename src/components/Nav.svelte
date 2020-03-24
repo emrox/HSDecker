94a472a4ab8300c4c5b16ca30f7372d677a6d5f3
Stefan Bauckmeier <emrox@elektronische-musik.de>
2020-03-24 17:20:28 +0100

    feat: sample register form on nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,6 +2,12 @@
   export let segment;
 </script>
 
+<style>
+  .dropdown-menu-login {
+    width: 300px;
+  }
+</style>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="/">HSDecker</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,6 +22,30 @@
       <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
           the blog data when we hover over the link or tap it on a touchscreen -->
       <li class="nav-item" class:active={segment === 'blog'}><a class="nav-link" href="blog">blog</a></li>
+    </ul>
+
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="/" id="navbarDropdownLogin" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          login
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-right dropdown-menu-login" aria-labelledby="navbarDropdownLogin">
+          <form class="px-4 py-8">
+            <div class="form-group">
+              <label for="exampleDropdownFormEmail1">Email address</label>
+              <input type="email" class="form-control" placeholder="email@example.com">
+            </div>
+
+            <div class="form-group">
+              <label for="exampleDropdownFormPassword1">Password</label>
+              <input type="password" class="form-control" placeholder="Password">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Register</button>
+          </form>
+        </div>
+      </li>
     </ul>
   </div>
 </nav>
