44b1e9a54505959da12d24c51c87dde52d6ebfc3
Stefan Bauckmeier <emrox@elektronische-musik.de>
2020-03-26 18:09:56 +0100

    feat: simple card name input box with dummy action

--- src/routes/card/index.svelte
+++ src/routes/card/index.svelte
@@ -1,1 +1,20 @@
-<div>Card Index</div>
+<script>
+  let cardName = '';
+  let matchedCards = [];
+
+  $: showMatchedCards(cardName);
+
+  const showMatchedCards = (cardName) => {
+    if (cardName.length <= 2) { return; }
+
+    matchedCards = [cardName.split('')];
+  }
+</script>
+
+<div>
+  <input type="text" bind:value={cardName} />
+
+  <div class="cardResults">
+    {matchedCards.join(' / ')}
+  </div>
+</div>
