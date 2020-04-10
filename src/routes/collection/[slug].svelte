3f7d7125b8aa8aaabe7b50c4e980bd8f2ab96031
Stefan Bauckmeier <emrox@elektronische-musik.de>
2020-04-10 18:08:47 +0200

    feat: simple dummy collection card controls

--- src/routes/collection/[slug].svelte
+++ src/routes/collection/[slug].svelte
@@ -41,8 +41,32 @@
 </script>
 
 <style>
+  .container {
+    margin-bottom: 20px;
+  }
+
   .hscard img {
     max-width: 100%;
+  }
+
+  .ctrl-container {
+    position: absolute;
+    display: flex;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    justify-content: center;
+  }
+
+  .ctrl-cnt {
+    width: 40px;
+    cursor: default;
+    color: #000;
+  }
+
+  .ctrl-plus,
+  .ctrl-minus {
+    width: 30px;
   }
 </style>
 
@@ -53,6 +77,14 @@
       {#each displayCards as classCard}
         <div class="col-3 hscard">
           <img src="https://art.hearthstonejson.com/v1/render/latest/enUS/256x/{classCard.id}.png" alt="{classCard.name}" />
+
+          <div class="ctrl-container">
+            <div class="btn-group" role="group" aria-label="Basic example">
+              <button type="button" class="btn btn-sm btn-warning ctrl-plus">+</button>
+              <button type="button" class="btn btn-sm btn-light ctrl-cnt" disabled>0</button>
+              <button type="button" class="btn btn-sm btn-warning ctrl-minus">-</button>
+            </div>
+          </div>
         </div>
       {/each}
   </div>
