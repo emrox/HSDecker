d81435ae2a288208cfb5ad8377e4ed69f49880c3
Stefan Bauckmeier <emrox@elektronische-musik.de>
2020-04-20 12:08:27 +0200

    feat: show Dust cost for user considering users collected cards

--- src/components/Deck/DeckDust.svelte
+++ src/components/Deck/DeckDust.svelte
@@ -1,4 +1,7 @@
 <script>
+  import { currentUser } from '../../stores/currentUser.store';
+  import { userCards } from '../../stores/userCards.store';
+
   export let deck;
 
   const costMapping = { 'FREE': 0, 'COMMON': 40, 'RARE': 100, 'EPIC': 400, 'LEGENDARY': 1600 };
@@ -6,8 +9,28 @@
   const fullManaCost = deck.reduce((manaSum, { card, count }) => {
     return manaSum + (costMapping[card.rarity] * count);
   }, 0);
+
+  let userManaCost = fullManaCost;
+
+  userCards.subscribe(ucards => {
+    userManaCost = deck.reduce((manaSum, { card, count }) => {
+      const needCount = count - (ucards[card.id] || 0);
+
+      if (needCount <= 0) { return manaSum; }
+
+      return manaSum + (costMapping[card.rarity] * needCount);
+    }, 0);
+  });
 </script>
 
 <div>
-  Dust cost: {fullManaCost} <img src="/img/dust.png" alt="dust" />
+  Dust cost:
+  
+  {userManaCost}
+
+  {#if fullManaCost !== userManaCost}
+    ({fullManaCost})
+  {/if}
+
+  <img src="/img/dust.png" alt="dust" />
 </div>
